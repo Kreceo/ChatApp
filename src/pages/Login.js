faaf9f8358c0283f70a4aa366a09afda06b9fec0
Darrell Lane <darrell-lane@hotmail.co.uk>
2021-03-21 19:27:48 +0000

    Refactor ( Styling updates to signup & login )

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -50,37 +50,40 @@ export default class Login extends Component {
   
   render() {
     return (
-      <div>
+      <div className="d-flex full-height justify-content-center align-items-center">
+        <div className="p-4 shadow p-3 mb-5 bg-white rounded">
         <form autoComplete="off" onSubmit={this.handleSubmit}>
           <h1>Login to 
-              <Link to="/"> Chatty </Link>
+              <Link to="/"> ChitChat </Link>
           </h1>
           <p>Fill in the form below to login to your account.</p>
-          <div>
-            <input placeholder="Email" name="email" type="email" onChange={this.handleChange} value={this.state.email}></input>
+          <div className="mb-3">
+            <input placeholder="Email Address" name="email" type="email" className="form-control w-100" onChange={this.handleChange} value={this.state.email}></input>
           </div>
-          <div>
-            <input placeholder="Password" name="password" onChange={this.handleChange} value={this.state.password} type="password"></input>
+          <div className="mb-3">
+            <input placeholder="Password" name="password" className="form-control w-100" onChange={this.handleChange} value={this.state.password} type="password"></input>
           </div>
           <div>
             {this.state.error ? (
               <p>{this.state.error}</p>
             ) : null}
-            <button type="submit">Login</button>
+            <button type="submit" className="btn btn-primary">Login</button>
           </div>
+          <hr />
           <p>You can also log in with any of these services</p>
-          <button className="btn btn-danger mr-2" type="button" onClick={this.googleSignIn}>
+          <button className="btn btn-info w-100 mb-2" type="button" onClick={this.googleSignIn}>
             Sign in with Google
           </button>
-          <button className="btn btn-secondary" type="button" onClick={this.githubSignIn}>
+          <button className="btn btn-secondary w-100" type="button" onClick={this.githubSignIn}>
             Sign in with GitHub
           </button>
           <hr />
           <p>
             Don't have an account? 
-            <Link to="/signup">Sign up</Link>
+            <Link to="/signup"> Sign up</Link>
           </p>
         </form>
+        </div>
       </div>
     );
   }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -51,33 +51,40 @@ export default class SignUp extends Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h1>
-            Sign Up to
-          <Link to="/">Chatty</Link>
+      <div className="d-flex full-height justify-content-center align-items-center">
+        <div className="p-4 shadow p-3 mb-5 bg-white rounded">
+         <form autoComplete="off" onSubmit={this.handleSubmit}>
+          <h1>Sign up to 
+              <Link to="/"> ChitChat </Link>
           </h1>
-          <p>Fill in the form below to create an account.</p>
-          <div>
-            <input placeholder="Email" name="email" type="email" onChange={this.handleChange} value={this.state.email}></input>
+          <p>Fill in the form below to create your account.</p>
+          <div className="mb-3">
+            <input placeholder="Email Address" name="email" type="email" className="form-control w-100" onChange={this.handleChange} value={this.state.email}></input>
+          </div>
+          <div className="mb-3">
+            <input placeholder="Password" name="password" className="form-control w-100" onChange={this.handleChange} value={this.state.password} type="password"></input>
           </div>
           <div>
-            <input placeholder="Password" name="password" onChange={this.handleChange} value={this.state.password} type="password"></input>
+            {this.state.error ? (
+              <p>{this.state.error}</p>
+            ) : null}
+            <button type="submit" className="btn btn-primary mb-2">Create account</button>
           </div>
-          <div>
-            {this.state.error ? <p>{this.state.error}</p> : null}
-            <button type="submit">Sign up</button>
-          </div>
-          <p>Or</p>
-            <button onClick={this.googleSignIn} type="button">
-                Sign up with Google
-            </button><br></br>
-            <button type="button" onClick={this.githubSignIn}>
-                Sign up with GitHub
-            </button>
-          <hr></hr>
-          <p>Already have an account? <Link to="/login">Login</Link></p>
+          <hr />
+          <p>You can also log in with any of these services</p>
+          <button className="btn btn-info w-100 mb-2" type="button" onClick={this.googleSignIn}>
+            Sign up with Google
+          </button>
+          <button className="btn btn-secondary w-100" type="button" onClick={this.githubSignIn}>
+            Sign up with GitHub
+          </button>
+          <hr />
+          <p>
+              Already registered? 
+            <Link to="/login"> Login</Link>
+          </p>
         </form>
+        </div>
       </div>
     )
   }
