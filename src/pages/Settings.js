3e1dca1156e01fdea0462a273d591d54bd3dffdc
Darrell Lane <darrell-lane@hotmail.co.uk>
2021-03-28 11:31:32 +0100

    Refactor ( Settings design page )

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -21,9 +21,7 @@ export default class HomePage extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    
     alert('Your username is: ' + this.input.value);
-    
   };
 
   render() {
@@ -31,37 +29,72 @@ export default class HomePage extends Component {
       <div className="d-flex h-100">
         <Header/>
         <section className="d-flex full-height justify-content-center align-items-center width90 m-auto">
-        <form onSubmit={this.handleSubmit}>
+          <div className="profile-container">
           <h2>Profile Information</h2>
           <h4>Update / add your information below:</h4>
-          <div className="mb-3">
-          <div>
-            <img src={this.state.user.photoURL} className="rounded-circle"/>
+          <hr/>
+            <div className="row">
+              <div className="col-md-4 mb-3">
+                <div className="card h-100 mw-100 border-shadow">
+                  <div className="card-body">
+                    <div className="d-flex flex-column align-items-center text-center">
+                      <img src={this.state.user.photoURL} alt="Admin" className="rounded-circle" width="150" />
+                      <div className="mt-3">
+                        <h4>{this.state.user.displayName}</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-8">
+                <div className="card mb-3 mw-100">
+                <form onSubmit={this.handleSubmit} className="border-shadow">
+                  <div className="card-body">
+                    <div className="align-items-center">
+                      <h6 className="pt-2">Full Name</h6>
+                      <input
+                        type="text"
+                        className="form-control w-100"
+                        name="name"
+                        defaultValue={this.state.user.displayName}
+                        onChange={this.handleChange}
+                        ref={(input) => this.input = input}
+                      />
+                    </div>
+                    <hr/>
+                    <div className="align-items-center">
+                      <h6 className="pt-2">Username</h6>
+                      <input
+                        type="text"
+                        className="form-control w-100"
+                        name="username"
+                        defaultValue={this.state.user.displayName}
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <h3>{this.state.username}</h3>
+                    <hr/>
+                    <div className="align-items-center">
+                      <h6 className="pt-2">Email</h6>
+                      <input
+                        type="email"
+                        className="form-control w-100"
+                        name="email"
+                        defaultValue={this.state.user.email}
+                        onChange={this.handleChange}
+                        ref={(input) => this.input = input}
+                      />
+                    </div>
+                    <h3>{this.state.email}</h3>
+                  </div>
+                  <div className="card-body">
+                    <button type="submit" className="btn btn-primary">Update details</button>
+                  </div>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
-            <label htmlFor="username" className="form-label">Username</label>
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              defaultValue={this.state.user.displayName}
-              onChange={this.handleChange}
-            />
-          </div>
-          <h3>{this.state.username}</h3>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">email</label>
-            <input
-              type="email"
-              className="form-control"
-              name="email"
-              defaultValue={this.state.user.email}
-              onChange={this.handleChange}
-              ref={(input) => this.input = input}
-            />
-          </div>
-          <h3>{this.state.email}</h3>
-          <button type="submit" className="btn btn-primary">Update details</button>
-        </form>
         </section>
         <Footer/>
       </div>
